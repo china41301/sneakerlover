c3b5713abdc00c7608a84042eb798d3a451e2dae
BG389966 <djcao@best-inc.com>
2019-07-18 13:07:36 +0800

    打印日志

---
+++ web/src/main/java/com/djcao/boot/aspect/LoggerAspect.java
@@ -0,0 +1,38 @@
+package com.djcao.boot.aspect;
+
+import com.alibaba.fastjson.JSON;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author djcao
+ * @workcode BG389966
+ * @date 2019/7/18
+ */
+@Aspect
+public class LoggerAspect {
+
+    Logger logger = LoggerFactory.getLogger(LoggerAspect.class);
+
+    @Pointcut("execution(public * com.djcao.boot.controller.*.*( .. ))")
+    public void pointcut(){
+    }
+
+    @Around(value = "pointcut()")
+    public Object aroutd(ProceedingJoinPoint pjp) throws Throwable {
+        try {
+            logger.info(pjp.getTarget().getClass().getSimpleName()+"."+pjp.getSignature().getName
+                ()+" args:"+JSON.toJSONString(pjp.getArgs()));
+            Object proceed = pjp.proceed();
+            logger.info(JSON.toJSONString(proceed));
+            return proceed;
+        } catch (Throwable throwable) {
+            logger.error("loggerAspect",throwable);
+            throw throwable;
+        }
+    }
+}
