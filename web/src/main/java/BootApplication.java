0c6e15d5faf0493730abf20528f15ae8fccddac9
changfaaipiaorou <974441301@qq.com>
2019-07-19 22:58:55 +0800

    增加spring session和redis

--- web/src/main/java/BootApplication.java
+++ web/src/main/java/BootApplication.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EntityScan(basePackages = "com.djcao.boot.repository")
 @EnableSwagger2
 @EnableRedisHttpSession
+@EnableCaching
 public class BootApplication {
 
 	public static void main(String[] args) {
