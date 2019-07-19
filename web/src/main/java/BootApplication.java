7be2bd21f888c3a70b2aca3f139ca6766ef7f65a
changfaaipiaorou <974441301@qq.com>
2019-07-19 23:13:13 +0800

    增加spring session和redis

--- web/src/main/java/BootApplication.java
+++ web/src/main/java/BootApplication.java
@@ -1,11 +1,8 @@
-import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.client.RestTemplate;
